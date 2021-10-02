b5202bc20da53b0d0b9879ea940a9566657740e9
Andrew Vaimon <niko142000@mail.ru>
2021-10-02 14:08:22 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/chinese_checkers.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chinese_checkers.dir/main.cpp.obj"
+  "chinese_checkers.exe"
+  "chinese_checkers.exe.manifest"
+  "chinese_checkers.lib"
+  "chinese_checkers.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chinese_checkers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/chinese_checkers.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for chinese_checkers.
+# This may be replaced when dependencies are built.
