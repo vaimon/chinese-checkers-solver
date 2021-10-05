39ba4d5a23eee1838cc1487c73c75fe642731636
Andrew Vaimon <niko142000@mail.ru>
2021-10-05 22:05:56 +0300

    checkpoint

--- Game.cpp
+++ Game.cpp
@@ -4,15 +4,15 @@
 
 #include "Game.h"
 
-Game::Game(){
- finiteState[4][4] = true;
+Game::Game() {
+    finiteState[4][4] = true;
 }
 
-Field Game::getInitialField() {
-    Field res {};
-    for(int i = 0; i < 9; i++){
+GameField Game::getInitialField() {
+    GameField res{};
+    for (int i = 0; i < 9; i++) {
         int j = 0, k = 9;
-        if(i > 2 && i < 6){
+        if (!(i > 2 && i < 6)) {
             j = 3;
             k = 6;
         }
@@ -20,19 +20,96 @@ Field Game::getInitialField() {
             res[i][j] = true;
         }
     }
+    res[4][4] = false;
     return res;
 }
 
 void Game::solve() {
-    Field f = getInitialField();
+    GameField f = getInitialField();
+    getWalkingMatrices(f);
     printField(f);
 }
 
-void Game::printField(std::array<std::array<bool, 9>, 9> f) {
+void Game::printField(GameField f) {
     for (int i = 0; i < 9; ++i) {
         for (int j = 0; j < 9; ++j) {
-            std::cout << (f[i][j] ? "●" : "○") << "";
+            if(!(i > 2 && i < 6) && (j < 3 || j > 5)){
+                printf("   ");
+                continue;
+            }
+            if (f[i][j]) {
+                printf(" o ");
+            } else {
+                printf(" x ");
+            }
+
         }
-        std::cout<<std::endl;
+        std::cout << std::endl;
     }
+}
+
+std::pair<HorizontalField,VerticalField> Game::getWalkingMatrices(GameField &f) {
+    HorizontalField horizontal {};
+    VerticalField vertical {};
+
+    for (int i = 0; i < 9; ++i) {
+        for (int j = 3; j < 6; ++j) {
+            vertical[i][j-3] = &f[i][j];
+        }
+    }
+    for (int i = 3; i < 6; ++i) {
+        for (int j = 0; j < 9; ++j) {
+            horizontal[i-3][j] = &f[i][j];
+        }
+    }
+    return {horizontal,vertical};
+}
+
+void Game::printWalkingMatrices(HorizontalField horizontal,VerticalField vertical) {
+    std::cout << std::endl;
+    for (int i = 0; i < 9; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            if (*vertical[i][j]) {
+                printf(" o ");
+            } else {
+                printf(" x ");
+            }
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 9; ++j) {
+            if (*horizontal[i][j]) {
+                printf(" o ");
+            } else {
+                printf(" x ");
+            }
+        }
+        std::cout << std::endl;
+    }
+}
+
+std::vector<std::pair<int, int>> Game::getAvailableMoves(GameField& f) {
+    auto [horizontal, vertical] = getWalkingMatrices(f);
+    for (int i = 0; i < 9; ++i) {
+        
+
+    }
+}
+
+bool Game::checkUp(GameField &f, int i, int j) {
+    return false;
+}
+
+bool Game::checkDown(GameField &f, int i, int j) {
+    return false;
+}
+
+bool Game::checkRight(GameField &f, int i, int j) {
+    return false;
+}
+
+bool Game::checkLeft(GameField &f, int i, int j) {
+    return false;
 }
--- Game.h
+++ Game.h
@@ -5,22 +5,39 @@
 #ifndef CHINESE_CHECKERS_GAME_H
 #define CHINESE_CHECKERS_GAME_H
 
-#define Field std::array<std::array<bool,9>,9>
+#define GameField std::array<std::array<bool,9>,9>
+#define VerticalField std::array<std::array<bool*,3>,9>
+#define HorizontalField std::array<std::array<bool*,9>,3>
+
 #include <array>
 #include <iostream>
+#include <cstdio>
+#include <vector>
 
 class Game {
-    Field finiteState {};
-    std::array<std::array<bool*,9>,3> horizontal {};
-    std::array<std::array<bool*,3>,9> vertical {};
+    GameField finiteState {};
 public:
     Game();
 
-    static Field getInitialField();
+    static GameField getInitialField();
 
     void solve();
 
-    void printField(Field f);
+    static std::pair<std::array<std::array<bool*,9>,3>,std::array<std::array<bool*,3>,9>> getWalkingMatrices(GameField& f);
+
+    static void printField(GameField f);
+
+    static void printWalkingMatrices(std::array<std::array<bool*,9>,3> horizontal,std::array<std::array<bool*,3>,9> vertical);
+
+    static std::vector<std::pair<int,int>> getAvailableMoves(GameField& f);
+
+    static bool checkUp(GameField& f, int i, int j);
+
+    static bool checkDown(GameField& f, int i, int j);
+
+    static bool checkRight(GameField& f, int i, int j);
+
+    static bool checkLeft(GameField& f, int i, int j);
 };
 
 
